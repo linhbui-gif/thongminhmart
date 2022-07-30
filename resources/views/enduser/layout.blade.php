b245239fed481b944ddfcef461f658ed3b220961
Van Cong <trinhvancong1996@gmail.com>
2022-07-31 00:01:05 +0700

    ud

--- resources/views/enduser/layout.blade.php
+++ resources/views/enduser/layout.blade.php
@@ -8,8 +8,8 @@
 <body>
     <div id="top"></div>
     @include("enduser.components.header_desktop")
-    @include("enduser.components.navigation")
-    @include("enduser.components.notify")
+    {{--@include("enduser.components.navigation")
+    @include("enduser.components.notify")--}}
     @yield('content')
 
     @include("enduser.components.footer")
